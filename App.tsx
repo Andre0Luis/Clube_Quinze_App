74dfb0f13207f5c1c906f931d961004660dd1af4
Andre0Luis <aluis283@gmail.com>
2026-02-25 21:44:07 -0300

    chore: add sentry and ios build

--- App.tsx
+++ App.tsx
@@ -1,1 +1,25 @@
+import * as Sentry from "@sentry/react-native";
 import "expo-router/entry";
+
+Sentry.init({
+  dsn: "https://685bda8e0d370ae91fe753f5c9fd14d8@o4510949392646144.ingest.us.sentry.io/4510949401165824",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+  debug: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
